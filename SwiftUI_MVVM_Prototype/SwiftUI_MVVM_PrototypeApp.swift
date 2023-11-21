a1c2e20732c575264aabf2c0fd836478df78cb64
Giau Huynh <giauhc@hopee.com.vn>
2023-11-21 16:13:35 +0700

    Initial commit

---
+++ SwiftUI_MVVM_Prototype/SwiftUI_MVVM_PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVM_PrototypeApp.swift
+//  SwiftUI_MVVM_Prototype
+//
+//  Created by hopee on 21/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVM_PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI_MVVM_Prototype/Utility/Extension+.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+
+extension URL {
+    var formatted: String {
+        (host ?? "").replacingOccurrences(of: "www.", with: "")
+    }
+}
+
+extension Int {
+    var formatted: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        return formatter.string(from: NSNumber(value: self)) ?? ""
+    }
+}
+
+extension Date {
+    var timeAgo: String {
+        let formatter = RelativeDateTimeFormatter()
+        formatter.unitsStyle = .short
+        return formatter.localizedString(for: self, relativeTo: Date())
+    }
+}
+
+extension Color {
+    static var teal: Color {
+        Color(UIColor.systemTeal)
+    }
+}
